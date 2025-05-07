3fc15a394b6139404733972012ce7ad3be1d91fb
VK132211 <m.vk.80899@gmail.com>
2025-05-07 11:51:09 +0530

    implement CRUD operations

---
+++ src/posts/posts.service.ts
@@ -0,0 +1,68 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Post } from './interfaces/post.interface';
+
+@Injectable()
+export class PostsService {
+  private posts: Post[] = [
+    {
+      id: 1,
+      title: 'First',
+      content: 'First Post',
+      authorName: 'Vinay',
+      createdAt: new Date(),
+    },
+  ];
+
+  findAll(): Post[] {
+    return this.posts;
+  }
+
+  findOne(id: number): Post {
+    const singlePost = this.posts.find((post) => post.id === id);
+    if (!singlePost) {
+      throw new NotFoundException(`Post with ID ${id} is not found`);
+    }
+    return singlePost;
+  }
+
+  create(createPostData: Omit<Post, 'id' | 'createdAt'>): Post {
+    const newPost: Post = {
+      id: this.getNextId(),
+      ...createPostData,
+      createdAt: new Date(),
+    };
+    this.posts.push(newPost);
+    return newPost;
+  }
+
+  update(
+    id: number,
+    updatePostData: Partial<Omit<Post, 'id' | 'createdAt'>>,
+  ): Post {
+    const currentPostIndexEdit = this.posts.findIndex((post) => post.id===id);
+    if (currentPostIndexEdit === -1) {
+      throw new NotFoundException(`Post with ID ${id} not found`);
+    }
+    this.posts[currentPostIndexEdit] = {
+      ...this.posts[currentPostIndexEdit],
+      ...updatePostData,
+      updatedAt: new Date(),
+    };
+    return this.posts[currentPostIndexEdit];
+  }
+
+  remove(id: number): { message: string } {
+    const currentPostIndexToDelete = this.posts.findIndex((post) => post.id===id);
+    if (currentPostIndexToDelete === -1) {
+      throw new NotFoundException(`Post with ID ${id} not found`);
+    }
+    this.posts.splice(currentPostIndexToDelete, 1);
+    return { message: `Post with ID ${id} has been deleted` };
+  }
+
+  private getNextId(): number {
+    return this.posts.length > 0
+      ? Math.max(...this.posts.map((post) => post.id)) + 1
+      : 1;
+  }
+}
